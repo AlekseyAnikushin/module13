79d2ae044bf95a547968da5b1bf3602268f86e03
Aleksey Anikushin <ania@automacon.ru>
2024-05-14 20:11:20 +0300

    Version v1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AlekseyAnikushin/module13
+
+go 1.22.1
---
+++ module13.go
@@ -0,0 +1,69 @@
+package module13
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Patient struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+func read(inputFile string, res *[]Patient) error {
+	f, err := os.Open(inputFile)
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	var p Patient
+	dec := json.NewDecoder(f)
+	*res = make([]Patient, 0, 3)
+	for dec.More() {
+		err = dec.Decode(&p)
+		if err != nil {
+			return err
+		}
+		*res = append(*res, p)
+	}
+
+	return nil
+}
+
+func write(outputFile string, data *[]Patient) error {
+	f, err := os.Create(outputFile)
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	enc := json.NewEncoder(f)
+	err = enc.Encode(data)
+
+	return err
+}
+
+func Do(inFile string, outFile string) error {
+	var p []Patient
+
+	err := read(inFile, &p)
+
+	if err != nil {
+		return err
+	}
+
+	if p != nil {
+		err = write(outFile, &p)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
